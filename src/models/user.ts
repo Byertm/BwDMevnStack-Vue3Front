7be320ce22d3a0dca5b47a192c1003cd8109b98b
Ersin Biltekin <ersin.biltekin.57@gmail.com>
2023-03-27 07:00:24 +0300

    Email Regex Added

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,7 @@
 import { IDocument, IRole } from '@models/index';
 
+export const emailRegex = RegExp(/^[\w-\._\+%]+@(live|hotmail|outlook|aol|yahoo|rocketmail|gmail|gmx\.com|mail.com|inbox.com|icloud|aim|yandex|zoho)\./);
+
 export interface IUser extends IDocument {
 	first_name: string;
 	last_name: string;
